dc59436d4d8936656eb963d577558416bd1c8a9b
Sohaib Afifi <sohaib.lafifi@univ-artois.fr>
2026-02-23 12:44:59 +0100

    Added MARCO smart-core and adaptive mode entry point

---
+++ marco_smart.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import sys
+
+from src.marco.marco import parse_args, enumerate_with_args
+
+
+def main():
+    args_list = sys.argv[1:]
+    if '--adaptive' not in args_list:
+        args_list = ['--adaptive'] + args_list
+    if '--smart-core' not in args_list:
+        args_list = ['--smart-core'] + args_list
+    args = parse_args(args_list)
+    for result in enumerate_with_args(args, print_results=True):
+        print(result)
+
+
+if __name__ == '__main__':
+    main()
